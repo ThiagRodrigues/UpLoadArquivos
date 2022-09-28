71bb3b0ad4ce44e61c875e37964486be40b2537f
ThiagRodrigues <114624222+ThiagRodrigues@users.noreply.github.com>
2022-09-28 19:28:32 -0300

    Initial commit

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpLoadArquivos.Controllers
+{
+    public class UploadController : Controller
+    {
+        IHostingEnvironment _appEnvironment;
+        public UploadController(IHostingEnvironment env)
+        {
+            _appEnvironment = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> EnviarArquivo(List<IFormFile> arquivos)
+        {
+            string msg = "Formato não é valido";
+            
+            long tamanhoArquivos = arquivos.Sum(f => f.Length);
+            var caminhoArquivo = Path.GetTempFileName();
+
+            foreach (var arquivo in arquivos)
+            {
+                if (arquivo == null || arquivo.Length == 0)
+                {
+                    ViewData["Erro"] = "Error: Arquivo não selecionado";
+                    return View(ViewData);
+                }
+
+                string pasta = "Arquivos_Usuario";
+                string nomeArquivo = "Usuario_arquivo_" + DateTime.Now.Millisecond.ToString();
+
+                if (arquivo.FileName.Contains(".xls"))
+                    nomeArquivo += ".xls";
+                else if (arquivo.FileName.Contains(".csv"))
+                    nomeArquivo += ".csv";
+                else if (arquivo.FileName.Contains(".xml"))
+                    nomeArquivo += ".xml";
+                else if (arquivo.FileName.Contains(".pdf"))
+                    nomeArquivo += ".pdf";
+                else if (arquivo.FileName.Contains(".json"))
+                    nomeArquivo += ".json";
+                else
+                    throw new ArgumentOutOfRangeException(null, msg);
+
+
+                string caminho_WebRoot = _appEnvironment.WebRootPath;
+                string caminhoDestinoArquivo = caminho_WebRoot + "\\Arquivos\\" + pasta + "\\";
+                string caminhoDestinoArquivoOriginal = caminhoDestinoArquivo + "\\Recebidos\\" + nomeArquivo;
+
+                using (var stream = new FileStream(caminhoDestinoArquivoOriginal, FileMode.Create))
+                {
+                    await arquivo.CopyToAsync(stream);
+                }
+            }
+
+            ViewData["Resultado"] = $"{arquivos.Count} arquivo enviado, " +
+             $"com tamanho total de : {tamanhoArquivos} bytes";
+
+            return View(ViewData);
+        }
+    }
+}
---
+++ README_TesteUpper.txt
@@ -0,0 +1,14 @@
+Para acessar o projeto via localhost
+
+https://localhost:44341/Upload
+
+* Introdução da solução Proposta
+
+Implementado um Metodo que receba os tipos de arquivos: XLS, CSV, XML, PDF e JSON
+Nesta etapa, recebemos os arquivos e adicionamos em uma pasta para que seja processado por um software "XPTO"
+Em um segundo momento, será necessário refatorar o processo do recebimento dos arquivos e converter para JSON, 
+pois é um formato leve e de grande utilização para gerar Relatórios(BI). 
+Desta forma vejo a resolução do problema, deixo ciente que, mesmo sendo um teste a principio. 
+Não temos de fato algo que seja concreto, ex. Energia, Água
+Sempre será recebido um pdf, agora outras contas de fato vejo que receberemos sempre XML. 
+Mas a proposta será futuramente converter tudo para Json. Assim teremos uma solução para gerar relatórios e graficos
